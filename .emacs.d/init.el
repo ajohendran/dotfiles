f8cf95f636556b7d85d423efb1d3d42c5b74a9f4
Aravindh Johendran <ajohendran@gmail.com>
2023-05-09 20:54:00 -0400

    Customizations

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -32,7 +32,7 @@
 ;;;; appearance customizations
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 (menu-bar-mode -1)
-(setq-default left-margin-width 1) ; Define new widths.
+(setq-default left-margin-width 0) ; Define new widths.
 (set-window-buffer nil (current-buffer)) ; Use them now.
 (set-frame-font "PragmataPro Liga 14" nil t)
 ;; (set-face-foreground 'font-lock-comment-face "#6A9955")
@@ -43,6 +43,8 @@
 ;;;; general manual customizations
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 (blink-cursor-mode 0)
+(column-number-mode 1)
+(scroll-bar-mode 0)
 (setq inhibit-startup-screen t)
 (setq ring-bell-function 'ignore)
 (windmove-default-keybindings)
@@ -105,6 +107,52 @@
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 (setq inferior-lisp-program "ccl64")
 (setq scheme-program-name   "/usr/local/bin/mit-scheme")
+
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;; Help with preferring to split windows side by side
+;;;; Consult buffers are more ueful that way
+;;;; On iPad BlinkSh terminals, emacs prefers to split up/down
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Fix annoying vertical window splitting.
+;; https://lists.gnu.org/archive/html/help-gnu-emacs/2015-08/msg00339.html
+(with-eval-after-load "window"
+  (defcustom split-window-below nil
+    "If non-nil, vertical splits produce new windows below."
+    :group 'windows
+    :type 'boolean)
+
+  (defcustom split-window-right nil
+    "If non-nil, horizontal splits produce new windows to the right."
+    :group 'windows
+    :type 'boolean)
+
+  (fmakunbound #'split-window-sensibly)
+
+  (defun split-window-sensibly
+      (&optional window)
+    (setq window (or window (selected-window)))
+    (or (and (window-splittable-p window t)
+             ;; Split window horizontally.
+             (split-window window nil (if split-window-right 'left  'right)))
+        (and (window-splittable-p window)
+             ;; Split window vertically.
+             (split-window window nil (if split-window-below 'above 'below)))
+        (and (eq window (frame-root-window (window-frame window)))
+             (not (window-minibuffer-p window))
+             ;; If WINDOW is the only window on its frame and is not the
+             ;; minibuffer window, try to split it horizontally disregarding the
+             ;; value of `split-width-threshold'.
+             (let ((split-width-threshold 0))
+               (when (window-splittable-p window t)
+                 (split-window window nil (if split-window-right
+                                              'left
+                                            'right))))))))
+
+(setq-default split-height-threshold  80
+              split-width-threshold   160) ; the reasonable limit for horizontal splits
+
 
 
 
@@ -341,7 +389,7 @@
   (lambda-themes-set-variable-pitch nil) 
   :config
   ;; load preferred theme
-  (load-theme 'lambda-light))
+  (load-theme 'lambda-dark))
 
 (use-package magit
   :ensure t
@@ -369,8 +417,8 @@
 		   partial-completion))))
   (orderless-component-separator 'orderless-escapable-split-on-space)
   (orderless-matching-styles
-   '(orderless-prefixes
-     orderless-literal     
+   '(orderless-literal
+     orderless-prefixes     
      orderless-regexp
      orderless-initialism
      ;; orderless-flex
--- .zshrc
+++ .zshrc
@@ -4,7 +4,7 @@ alias l="ls"
 alias ll="ls -lhG"
 alias lla="ls -lhaG"
 alias ec="emacsclient -t -e '(when (get-buffer \"*eshell*\") (switch-to-buffer \"*eshell*\"))'"
-alias ecd="emacsclient -t -s dev -e '(when (get-buffer \"*eshell*\") (switch-to-buffer \"*eshell*\"))'"
+alias ecd="emacsclient -t -s dev"
 alias gitcfg='git --git-dir=$HOME/.cfg/.git/ --work-tree=$HOME'
 alias rm="rm -i"
 alias mv="mv -i"
