901be79151e8c2859d9a24a96dddc1c617bbe24c
Aravindh Johendran <ajohendran@gmail.com>
2023-05-13 20:08:28 -0400

    Added emacs custom init file

---
+++ .emacs.d/custom.el
@@ -0,0 +1,23 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(connection-local-criteria-alist
+   '(((:application eshell)
+      eshell-connection-default-profile)))
+ '(connection-local-profile-alist
+   '((eshell-connection-default-profile
+      (eshell-path-env-list))))
+ '(custom-safe-themes
+   '("d43860349c9f7a5b96a090ecf5f698ff23a8eb49cd1e5c8a83bb2068f24ea563" "0f220ea77c6355c411508e71225680ecb3e308b4858ef6c8326089d9ea94b86f" ""))
+ '(package-selected-packages
+   '(ace-window consult-dir embark embark-consult rainbow-mode doom-themes github-dark-vscode-theme orderless multiple-cursors avy consult consult-eglot consult-project-extra magit marginalia vertico benchmark-init inf-clojure clojure-mode company paredit kaolin-themes vscode-dark-plus-theme))
+ '(safe-local-variable-values '((eval when (fboundp 'rainbow-mode) (rainbow-mode 1)))))
+
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
