530931dfb16591362b20fbc8e029cc8d2767ab59
Aravindh Johendran <ajohendran@gmail.com>
2024-09-16 06:18:55 +0000

    Add an emacs scratch file

---
+++ .emacs.d/scratch.el
@@ -0,0 +1,27 @@
+;; This buffer is for text that is not saved, and for Lisp evaluation.
+;; To create a file, visit it with C-x C-f and enter text in its buffer.
+
+
+(global-font-lock-mode 1)
+
+
+(set-background-color "1C1C1C")
+
+
+
+
+(face-attribute 'default :foreground)
+
+(set-face-background 'default "#18181B")
+
+;;(set-face-attribute :background 'default "1D1D1D")
+
+(set-face-foreground 'font-lock-string-face "#6F0373")
+light #6F0373 #8c047C
+dark #B3818E
+
+
+
+(face-attribute 'font-lock-string-face :foreground)
+kao li #39854C
+ko br #845a84
